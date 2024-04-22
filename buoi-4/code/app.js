ac6d1e624cf144ae3ef409f796c216d0201d871b
Đỗ Cao Long <long@myspa.vn>
2024-04-22 21:15:09 +0700

    [UPDATE] buoi 4

---
+++ buoi-4/code/app.js
@@ -0,0 +1,63 @@
+// let count = 1;
+// const n = count++; // 1 postfix, return first, then increase
+// console.log('n :>> ', n);
+// console.log('count :>> ', count);
+
+// let count2 = 1;
+// const n2 = ++count2; // 2 prefix, increase first, then return
+// console.log('n2 :>> ', n2);
+// console.log('count2 :>> ', count2);
+
+// console.log((0.1).toFixed(20)); 
+// console.log((0.2).toFixed(20));
+// console.log((0.1 + 0.2).toFixed(20));
+// console.log((0.3).toFixed(20));
+// console.log(0.1 + 0.2 == 0.3);
+// console.log(0.1 + 0.2 === 0.3);
+// console.log(Math.abs(0.1 + 0.2 - 0.3) < Number.EPSILON);
+// console.log((0.1 + 0.2).toFixed(1) === (0.3).toFixed(1));
+
+// function randomNumber(n){
+//     if(n <= 0) return -1;
+//     const random = Math.random() * n
+//     return Math.round(random)
+// }
+
+// console.log(randomNumber(10));
+// console.log(randomNumber(10));
+// console.log(randomNumber(10));
+// console.log(randomNumber(10));
+// console.log(randomNumber(10));
+// console.log(randomNumber(10));
+// console.log(randomNumber(10));
+// console.log(randomNumber(10));
+// console.log(randomNumber(10));
+// console.log(randomNumber(10));
+// console.log(randomNumber(10));
+// console.log(randomNumber(10));
+// console.log(randomNumber(10));
+// console.log(randomNumber(10));
+// console.log(randomNumber(10));
+
+// min: a
+// range: b - a
+// [10 , 100] = 90 
+// function numberRamdom(a, b) {
+//     if(a >= b) return -1;
+//     // const random = 
+//     return Math.round(Math.random() * (b - a)) + a
+// }
+
+
+// console.log(numberRamdom(100, 1000));
+// console.log(numberRamdom(100, 1000));
+// console.log(numberRamdom(100, 1000));
+// console.log(numberRamdom(100, 1000));
+
+//  const random = Math.random() * n;
+
+// console.log(Math.round(Math.random()))
+// console.log(Math.round(Math.random()))
+// console.log(Math.round(Math.random()))
+// console.log(Math.round(Math.random()))
+// console.log(Math.round(Math.random()))
